01e5784f51f15318f86b9c37645d8b72b11fe197
Arnaud Morin <arnaud.morin@gmail.com>
2017-12-08 21:52:34 +0100

    Add guirlande.py

---
+++ guirlande.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from nanpy import SerialManager
+from nanpy import ArduinoApi
+connection = SerialManager(device='/dev/ttyACM0')
+a = ArduinoApi(connection=connection)
+
+RED=5
+GREEN=6
+BLUE=3
+
+def init():
+    a.pinMode(RED, a.OUTPUT)
+    a.pinMode(GREEN, a.OUTPUT)
+    a.pinMode(BLUE, a.OUTPUT)
+
+def setColor(r, g, b):
+    a.analogWrite(RED, r)
+    a.analogWrite(GREEN, g)
+    a.analogWrite(BLUE, b)
+
+def red():
+    setColor(255, 0, 0)
+
+def green():
+    setColor(0, 255, 0)
+
+def blue():
+    setColor(0, 0, 255)
+
+setColor(255, 0, 0)
+blue()
+
