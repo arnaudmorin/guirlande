e161eec90ec9ee52c488fd5e2234d3e9302c3720
Arnaud Morin <arnaud.morin@corp.ovh.com>
2017-12-10 21:36:09 +0100

    Add requirements and pep8

--- guirlande.py
+++ guirlande.py
@@ -5,29 +5,34 @@ from nanpy import ArduinoApi
 connection = SerialManager(device='/dev/ttyACM0')
 a = ArduinoApi(connection=connection)
 
-RED=3
-GREEN=5
-BLUE=6
+RED = 3
+GREEN = 5
+BLUE = 6
+
 
 def init():
     a.pinMode(RED, a.OUTPUT)
     a.pinMode(GREEN, a.OUTPUT)
     a.pinMode(BLUE, a.OUTPUT)
 
+
 def setColor(r, g, b):
     a.analogWrite(RED, r)
     a.analogWrite(GREEN, g)
     a.analogWrite(BLUE, b)
 
+
 def red():
     setColor(255, 0, 0)
+
 
 def green():
     setColor(0, 255, 0)
 
+
 def blue():
     setColor(0, 0, 255)
 
+
 setColor(255, 0, 0)
 red()
-
